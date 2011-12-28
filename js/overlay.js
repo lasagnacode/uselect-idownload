lasagnacode/uselect-idownload

commit ae251fd7c871375846f2426da5373b6b85101a04

Port to latest statemachine.js version

js/overlay.js
@@ -226,82 +226,79 @@ function Overlay() {
 	document.body.addEventListener('mouseup', handlers.mouseup, true);
 
 	var statemachine = new StateMachine();
-	var state;
-
 /******************************************************************************/
-	state = new State('load');
+	statemachine.states['load'] = {
+		__enter__: function () {
+			document.documentElement.classList.add(CSS.classes.loading);
+			document.body.appendChild(overlay);
+			document.body.appendChild(glass);
+			document.body.appendChild(helpDiv);
 
-	state.setEnter(function () {
-		document.documentElement.classList.add(CSS.classes.loading);
-		document.body.appendChild(overlay);
-		document.body.appendChild(glass);
-		document.body.appendChild(helpDiv);
+			overlay.addEventListener('webkitTransitionEnd', function () {
+				overlay.removeEventListener('webkitTransitionEnd', arguments.callee);
+				that.sm.fireEvent('load_done');
+			});
 
-		overlay.addEventListener('webkitTransitionEnd', function () {
-			overlay.removeEventListener('webkitTransitionEnd', arguments.callee);
-			that.sm.fireEvent('load_done');
-		});
+			/*
+			 * We have to let the DOM to notice that the element has been added
+			 * before adding the class, otherwise the transition does not kick
+			 * in (the div is loaded already with opacity = 1)
+			 */
+			setTimeout(function () {
+				that.populate();
+				that.updateVisibleElements();
+				document.documentElement.classList.remove(CSS.classes.loading);
+			}, 0);
+		},
 
-		/*
-		 * We have to let the DOM to notice that the element has been added
-		 * before adding the class, otherwise the transition does not kick
-		 * in (the div is loaded already with opacity = 1)
-		 */
-		setTimeout(function () {
-			that.populate();
-			that.updateVisibleElements();
-			document.documentElement.classList.remove(CSS.classes.loading);
-		}, 0);
-	});
-
-	state.addEvent('load_done', 'idle');
-
-	statemachine.addState(state);
+		load_done: 'idle',
+	};
 /******************************************************************************/
-	state = new State('exit');
+	statemachine.states['exit'] = {
+		__enter__: function () {
+			document.removeEventListener('mousemove', handlers.mousemove);
+			document.removeEventListener('scroll', handlers.scroll);
+			window.removeEventListener('resize', handlers.resize);
+			document.removeEventListener('contextmenu', handlers.contextmenu);
 
-	state.setEnter(function () {
-		document.removeEventListener('mousemove', handlers.mousemove);
-		document.removeEventListener('scroll', handlers.scroll);
-		window.removeEventListener('resize', handlers.resize);
-		document.removeEventListener('contextmenu', handlers.contextmenu);
+			document.removeEventListener('keydown', handlers.keydown, true);
+			document.removeEventListener('keyup', handlers.keyup, true);
 
-		document.removeEventListener('keydown', handlers.keydown, true);
-		document.removeEventListener('keyup', handlers.keyup, true);
+			document.body.removeEventListener('mousedown', handlers.mousedown, true);
+			document.body.removeEventListener('mouseup', handlers.mouseup, true);
 
-		document.body.removeEventListener('mousedown', handlers.mousedown, true);
-		document.body.removeEventListener('mouseup', handlers.mouseup, true);
+			that._selectableElements.forEach(function (el) {
+				el._private.delegate.classList.remove(CSS.classes.selected);
+				el._private.delegate.classList.remove(CSS.classes.relative);
+				delete el._private;
+			});
 
-		that._selectableElements.forEach(function (el) {
-			el._private.delegate.classList.remove(CSS.classes.selected);
-			el._private.delegate.classList.remove(CSS.classes.relative);
-			delete el._private;
-		});
+			overlay.addEventListener('webkitTransitionEnd', function () {
+				overlay.removeEventListener('webkitTransitionEnd', arguments.callee);
+				document.body.removeChild(helpDiv);
+				document.body.removeChild(glass);
+				document.body.removeChild(overlay);
+				document.documentElement.classList.remove(CSS.classes.exiting);
+				that.sm.fireEvent('exit_done');
+			});
+			document.documentElement.classList.add(CSS.classes.exiting);
+		},
 
-		overlay.addEventListener('webkitTransitionEnd', function () {
-			overlay.removeEventListener('webkitTransitionEnd', arguments.callee);
-			document.body.removeChild(helpDiv);
-			document.body.removeChild(glass);
-			document.body.removeChild(overlay);
-			document.documentElement.classList.remove(CSS.classes.exiting);
-			that.sm.fireEvent('exit_done');
-		});
-		document.documentElement.classList.add(CSS.classes.exiting);
-	});
+		__exit__: function () {
+			delete Overlay.instance;
+		},
 
-	state.addEvent('exit_done', null);
-	statemachine.addState(state);
+		exit_done: null,
+	};
 /******************************************************************************/
-	state = new State('idle');
-
-	state.addEvent('mousedown', 'selection');
-	state.addEvent('alt_mousedown', 'deselection');
-	state.addEvent('req_exit', 'exit');
-	state.addEvent('req_open', 'open');
-	state.addEvent('req_download', 'download');
-	state.addEvent('hide_key_down', 'hidden');
-
-	statemachine.addState(state);
+	statemachine.states['idle'] = {
+		mousedown: 'selection',
+		alt_mousedown: 'deselection',
+		req_exit: 'exit',
+		req_open: 'open',
+		req_download: 'download',
+		hide_key_down: 'hidden',
+	};
 /******************************************************************************/
 	var redrawInterval = null;
 	var recalcInterval = null;
@@ -369,80 +366,69 @@ function Overlay() {
 		r.h = Math.abs(p0.y - p1.y);
 	};
 
-	state = new State('selection');
+	statemachine.states['selection'] = {
+		__enter__: function () {
+			that._invertedSelection = false;
+			selection_common_in();
+		},
 
-	state.setEnter(function () {
-		that._invertedSelection = false;
-		selection_common_in();
-	});
+		mousemove: selection_common_mousemove,
+		mouseup: selection_common_out,
+		req_exit: 'exit',
+	};
 
-	state.addEvent('mousemove', selection_common_mousemove);
-	state.addEvent('mouseup', selection_common_out);
-	state.addEvent('req_exit', 'exit');
+	statemachine.states['deselection'] = {
+		__enter__: function () {
+			that._invertedSelection = true;
+			selection_common_in();
+		},
 
-	statemachine.addState(state);
-
-	state = new State('deselection');
-
-	state.setEnter(function () {
-		that._invertedSelection = true;
-		selection_common_in();
-	});
-
-	state.addEvent('mousemove', selection_common_mousemove);
-	state.addEvent('alt_mouseup', selection_common_out);
-	state.addEvent('req_exit', 'exit');
-
-	statemachine.addState(state);
+		mousemove: selection_common_mousemove,
+		mouseup: selection_common_out,
+		req_exit: 'exit',
+	};
 /******************************************************************************/
-	state = new State('open');
+	statemachine.states['open'] = {
+		__enter__: function () {
+			clickElements(that._selectableElements.filter(function (el) {
+				return el._private.selected;
+			}), false);
+			that.sm.fireEvent('done');
+		},
 
-	state.setEnter(function () {
-		clickElements(that._selectableElements.filter(function (el) {
-			return el._private.selected;
-		}), false);
-		that.sm.fireEvent('done');
-	});
-
-	state.addEvent('done', 'exit');
-
-	statemachine.addState(state);
+		done: 'exit',
+	};
 /******************************************************************************/
-	state = new State('download');
+	statemachine.states['download'] = {
+		__enter__: function () {
+			clickElements(that._selectableElements.filter(function (el) {
+				return el._private.selected;
+			}), true);
+			that.sm.fireEvent('done');
+		},
 
-	state.setEnter(function () {
-		clickElements(that._selectableElements.filter(function (el) {
-			return el._private.selected;
-		}), true);
-		that.sm.fireEvent('done');
-	});
-
-	state.addEvent('done', 'exit');
-
-	statemachine.addState(state);
+		done: 'exit',
+	};
 /******************************************************************************/
-	state = new State('hidden');
+	statemachine.states['hidden'] = {
+		__enter__: function () {
+			document.documentElement.classList.add(CSS.classes.hidden);
+		},
 
-	state.setEnter(function () {
-		document.documentElement.classList.add(CSS.classes.hidden);
-	});
+		__exit__: function () {
+			document.documentElement.classList.remove(CSS.classes.hidden);
+			that.updateVisibleElements();
+		},
 
-	state.setExit(function () {
-		document.documentElement.classList.remove(CSS.classes.hidden);
-		that.updateVisibleElements();
-	});
-
-	state.addEvent('hide_key_up', 'idle');
-	state.addEvent('req_exit', 'exit');
-
-	statemachine.addState(state);
+		hide_key_up: 'idle',
+		req_exit: 'exit',
+	};
 /******************************************************************************/
-	statemachine.setInitialState('load');
 	this.sm = statemachine;
 }
 
 Overlay.prototype.show = function () {
-	this.sm.start();
+	this.sm.start('load');
 }
 
 Overlay.prototype.destroy = function () {

COPYING.LESSER
@@ -1,0 +1,165 @@
+                   GNU LESSER GENERAL PUBLIC LICENSE
+                       Version 3, 29 June 2007
+
+ Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>
+ Everyone is permitted to copy and distribute verbatim copies
+ of this license document, but changing it is not allowed.
+
+
+  This version of the GNU Lesser General Public License incorporates
+the terms and conditions of version 3 of the GNU General Public
+License, supplemented by the additional permissions listed below.
+
+  0. Additional Definitions.
+
+  As used herein, "this License" refers to version 3 of the GNU Lesser
+General Public License, and the "GNU GPL" refers to version 3 of the GNU
+General Public License.
+
+  "The Library" refers to a covered work governed by this License,
+other than an Application or a Combined Work as defined below.
+
+  An "Application" is any work that makes use of an interface provided
+by the Library, but which is not otherwise based on the Library.
+Defining a subclass of a class defined by the Library is deemed a mode
+of using an interface provided by the Library.
+
+  A "Combined Work" is a work produced by combining or linking an
+Application with the Library.  The particular version of the Library
+with which the Combined Work was made is also called the "Linked
+Version".
+
+  The "Minimal Corresponding Source" for a Combined Work means the
+Corresponding Source for the Combined Work, excluding any source code
+for portions of the Combined Work that, considered in isolation, are
+based on the Application, and not on the Linked Version.
+
+  The "Corresponding Application Code" for a Combined Work means the
+object code and/or source code for the Application, including any data
+and utility programs needed for reproducing the Combined Work from the
+Application, but excluding the System Libraries of the Combined Work.
+
+  1. Exception to Section 3 of the GNU GPL.
+
+  You may convey a covered work under sections 3 and 4 of this License
+without being bound by section 3 of the GNU GPL.
+
+  2. Conveying Modified Versions.
+
+  If you modify a copy of the Library, and, in your modifications, a
+facility refers to a function or data to be supplied by an Application
+that uses the facility (other than as an argument passed when the
+facility is invoked), then you may convey a copy of the modified
+version:
+
+   a) under this License, provided that you make a good faith effort to
+   ensure that, in the event an Application does not supply the
+   function or data, the facility still operates, and performs
+   whatever part of its purpose remains meaningful, or
+
+   b) under the GNU GPL, with none of the additional permissions of
+   this License applicable to that copy.
+
+  3. Object Code Incorporating Material from Library Header Files.
+
+  The object code form of an Application may incorporate material from
+a header file that is part of the Library.  You may convey such object
+code under terms of your choice, provided that, if the incorporated
+material is not limited to numerical parameters, data structure
+layouts and accessors, or small macros, inline functions and templates
+(ten or fewer lines in length), you do both of the following:
+
+   a) Give prominent notice with each copy of the object code that the
+   Library is used in it and that the Library and its use are
+   covered by this License.
+
+   b) Accompany the object code with a copy of the GNU GPL and this license
+   document.
+
+  4. Combined Works.
+
+  You may convey a Combined Work under terms of your choice that,
+taken together, effectively do not restrict modification of the
+portions of the Library contained in the Combined Work and reverse
+engineering for debugging such modifications, if you also do each of
+the following:
+
+   a) Give prominent notice with each copy of the Combined Work that
+   the Library is used in it and that the Library and its use are
+   covered by this License.
+
+   b) Accompany the Combined Work with a copy of the GNU GPL and this license
+   document.
+
+   c) For a Combined Work that displays copyright notices during
+   execution, include the copyright notice for the Library among
+   these notices, as well as a reference directing the user to the
+   copies of the GNU GPL and this license document.
+
+   d) Do one of the following:
+
+       0) Convey the Minimal Corresponding Source under the terms of this
+       License, and the Corresponding Application Code in a form
+       suitable for, and under terms that permit, the user to
+       recombine or relink the Application with a modified version of
+       the Linked Version to produce a modified Combined Work, in the
+       manner specified by section 6 of the GNU GPL for conveying
+       Corresponding Source.
+
+       1) Use a suitable shared library mechanism for linking with the
+       Library.  A suitable mechanism is one that (a) uses at run time
+       a copy of the Library already present on the user's computer
+       system, and (b) will operate properly with a modified version
+       of the Library that is interface-compatible with the Linked
+       Version.
+
+   e) Provide Installation Information, but only if you would otherwise
+   be required to provide such information under section 6 of the
+   GNU GPL, and only to the extent that such information is
+   necessary to install and execute a modified version of the
+   Combined Work produced by recombining or relinking the
+   Application with a modified version of the Linked Version. (If
+   you use option 4d0, the Installation Information must accompany
+   the Minimal Corresponding Source and Corresponding Application
+   Code. If you use option 4d1, you must provide the Installation
+   Information in the manner specified by section 6 of the GNU GPL
+   for conveying Corresponding Source.)
+
+  5. Combined Libraries.
+
+  You may place library facilities that are a work based on the
+Library side by side in a single library together with other library
+facilities that are not Applications and are not covered by this
+License, and convey such a combined library under terms of your
+choice, if you do both of the following:
+
+   a) Accompany the combined library with a copy of the same work based
+   on the Library, uncombined with any other library facilities,
+   conveyed under the terms of this License.
+
+   b) Give prominent notice with the combined library that part of it
+   is a work based on the Library, and explaining where to find the
+   accompanying uncombined form of the same work.
+
+  6. Revised Versions of the GNU Lesser General Public License.
+
+  The Free Software Foundation may publish revised and/or new versions
+of the GNU Lesser General Public License from time to time. Such new
+versions will be similar in spirit to the present version, but may
+differ in detail to address new problems or concerns.
+
+  Each version is given a distinguishing version number. If the
+Library as you received it specifies that a certain numbered version
+of the GNU Lesser General Public License "or any later version"
+applies to it, you have the option of following the terms and
+conditions either of that published version or of any later version
+published by the Free Software Foundation. If the Library as you
+received it does not specify a version number of the GNU Lesser
+General Public License, you may choose any version of the GNU Lesser
+General Public License ever published by the Free Software Foundation.
+
+  If the Library as you received it specifies that a proxy can decide
+whether future versions of the GNU Lesser General Public License shall
+apply, that proxy's public statement of acceptance of any version is
+permanent authorization for you to choose that version for the
+Library.

js/main.js
@@ -20,16 +20,14 @@
  * along with uSelect iDownload.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-browserActionClicked = (function () {
-	var overlay;
-
-	return function () {
-		if (overlay == undefined || overlay.sm.finished()) {
-			overlay = new Overlay();
-			overlay.show();
-		} else {
-			overlay.destroy();
-		}
+function browserActionClicked() {
+	if (Overlay.instance === undefined) {
+		var o = new Overlay();
+		Overlay.instance = o;
+		o.show();
+	} else {
+		Overlay.instance.destroy();
+		/* Overlay.instance deletion happens in overlay.js */
 	}
-})();
+}
 

js/statemachine.js
@@ -1,116 +1,149 @@
 /*
- * uSelect iDownload
+ * statemachine.js
+ * A simple state machine implementation for the Javascript programming
+ * language.
  *
  * Copyright © 2011 Alessandro Guido
  * Copyright © 2011 Marco Palumbo
  *
- * This file is part of uSelect iDownload.
- *
- * uSelect iDownload is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * uSelect iDownload is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with uSelect iDownload.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/**
+ * StateMachine constructor.
+ *
+ * @constructor
+ */
 function StateMachine() {
-	this._states = {};
-	this._initialState = undefined;
-	this._current = null;
+	/**
+	 * Collection of possible states.
+	 *
+	 * @public
+	 * @type {Object[]}
+	 */
+	this.states = {};
+
+	/**
+	 * Easily access the current state object.
+	 *
+	 * There are 2 special values:
+	 * - undefined: the state machine has not been started
+	 * - null: the state machine has terminated
+	 *
+	 * @private
+	 * @type {Object}
+	 */
+	this._current = undefined;
 }
 
-StateMachine.prototype.addState = function (state) {
-	this._states[state._name] = state;
-}
+/**
+ * Handlers in the form "__*__" have a special meaning and
+ * cannot be fired with StateMachine.fireEvent()
+ *
+ * @private
+ */
+StateMachine._restricted_event = /^__.*__$/;
 
-StateMachine.prototype._fireEvent = function (self, name, args) {
-	if (self._current == null)
-		throw "State machine not started or already terminated";
+/**
+ * Start the state machine.
+ *
+ * @this {StateMachine}
+ * @param {string} initialState Name of initial state.
+ */
+StateMachine.prototype.start = function (initialState) {
+	if (this._current !== undefined)
+		throw 'State machine already started';
+	if (initialState === undefined || initialState === null)
+		throw 'Please give a valid state name';
 
-	var state = self._states[self._current];
+	setTimeout(StateMachine._changeState, 0, this, initialState);
+};
 
-	var ret = state.fireEvent(name, args);
+/**
+ * Signal that an event has occurred.
+ *
+ * @this {StateMachine}
+ * @param {string} name Event name.
+ * @param arg Event argument (optional).
+ * @throws If state machine is not running or when firing a restricted event.
+ */
+StateMachine.prototype.fireEvent = function (name, arg) {
+	if (this._current === undefined)
+		throw 'State machine not started';
+	if (this._current === null)
+		throw 'State machine terminated';
+	if (StateMachine._restricted_event.test(name))
+		throw 'Firing event "' + name + '" is not allowed';
 
-	if (ret === undefined) // state unchanged
+	setTimeout(StateMachine._handleEvent, 0, this, name, arg);
+};
+
+/**
+ * Exit from the current state (if defined) and enter the next one.
+ * Should be called with asynchronously.
+ *
+ * @private
+ * @param {StateMachine} self StateMachine instance.
+ * @param {string|null} newstate Name of the state.
+ */
+StateMachine._changeState = function (self, newstate) {
+	if (newstate !== null && !self.states.hasOwnProperty(newstate)) {
+		self._current = null;
+		throw 'No such state "' + newstate + '"';
+	}
+
+	var curr = self._current;
+
+	if (curr !== undefined && curr.hasOwnProperty('__exit__')) {
+		/* this._current is undefined only when the state machine has
+		 * just been started */
+		curr.__exit__.apply(self);
+	}
+
+	if (newstate === null) {
+		/* state machine will terminate */
+		self._current = null;
 		return;
-	state.exit();
+	}
 
-	self._current = ret;
-	if (ret === null) // exit from statemachine
+	curr = self._current = self.states[newstate];
+
+	if (!curr.hasOwnProperty('__enter__'))
 		return;
 
-	if (!self._states.hasOwnProperty(ret))
-		throw "unknown state " + ret;
-	self._states[ret].enter();
-}
+	var next = curr.__enter__.apply(self);
+	if (next !== undefined)
+		setTimeout(StateMachine._changeState, 0, self, next);
+};
 
-StateMachine.prototype.fireEvent = function (name, args) {
-	setTimeout(this._fireEvent, 0, this, name, args);
-}
+/**
+ * Execute the event handler and if needed schedule a state change.
+ * Should be called asynchronously.
+ *
+ * @private
+ * @param {StateMachine} self StateMachine instance.
+ * @param {string} name Name of event.
+ * @param arg Event argument.
+ */
+StateMachine._handleEvent = function (self, name, arg) {
+	var next = self._current[name];
 
-StateMachine.prototype.setInitialState = function (state) {
-	this._initialState = state;
-}
+	if (typeof next == 'function')
+		next = next.apply(self, arg);
 
-StateMachine.prototype.start = function () {
-	if (this._initialState == undefined)
-		throw "initial state unset";
-	this._current = this._initialState;
-	setTimeout(function (self) {
-		self._states[self._current].enter();
-	}, 0, this);
-}
+	if (next === undefined) // state unchanged
+		return;
 
-StateMachine.prototype.current = function () {
-	return this._current;
-}
-
-StateMachine.prototype.finished = function () {
-    return this.current() == null;
-}
-
-
-function State(name) {
-	this._name = name;
-	this._enter = null;
-	this._exit = null;
-	this._events = {};
-}
-
-State.prototype.setEnter = function (fun) {
-	this._enter = fun;
-}
-
-State.prototype.setExit = function (fun) {
-	this._exit = fun;
-}
-
-State.prototype.enter = function () {
-	if (this._enter)
-		this._enter();
-}
-
-State.prototype.exit = function () {
-	if (this._exit)
-		this._exit();
-}
-
-State.prototype.addEvent = function (name, handler) {
-	this._events[name] = handler;
-}
-
-State.prototype.fireEvent = function (name, args) {
-	if (!this._events.hasOwnProperty(name))
-		return undefined;
-	var handler = this._events[name];
-	if (typeof handler == 'function')
-		return handler(args);
-	return handler;
-}
+	setTimeout(StateMachine._changeState, 0, self, next);
+};

commit 4d75fa299356e62dc06189259717c61a403f05f5

Fix recently introduced bug when deselecting items
This bug has been introduced in commit ae251fd7c871375846f2426da5373b6b85101a04

js/overlay.js
@@ -384,7 +384,7 @@ function Overlay() {
 		},
 
 		mousemove: selection_common_mousemove,
-		mouseup: selection_common_out,
+		alt_mouseup: selection_common_out,
 		req_exit: 'exit',
 	};
 /******************************************************************************/